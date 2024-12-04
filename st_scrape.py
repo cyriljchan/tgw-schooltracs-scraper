bd393b3c90e432ff898bd2cd603d5a01f341d4f5
Cyril Jay Chan <cyriljchan@yahoo.com>
2024-12-04 20:42:21 +0800

    feat: add timetable actions menu after login
    Menu shows actions you can perform with the timetable after logging in.

--- st_scrape.py
+++ st_scrape.py
@@ -1,6 +1,7 @@
 # Scrape SchoolTracs for student enrollment records
 # 'https://my.schooltracs.com/app/login'
 
+from datetime import datetime
 import getpass
 import os, sys
 from selenium import webdriver
@@ -40,13 +41,56 @@ class SchoolTracsFuncs:
         except:
             print("Logging in...")
             return True
+    
+    def change_branch(self):
+        # TODO: add functionality
+        pass
 
+    def change_date(self):
+        # TODO: add functionality
+        pass
 
 
 class SchoolTracsMain(SchoolTracsFuncs):
+    def __init__(self) -> None:
+        self.timetable = {"branch":"", "date":""}
+
     def timetable_menu(self):
-        # TODO: open timetable and print selectible functions
-        pass
+        os.system("cls")
+        timetable_btn = driver.find_element(By.XPATH, "//*[contains(text(), 'Timetable')]")
+        driver.execute_script("arguments[0].click()", timetable_btn)
+        self.timetable["branch"] = driver.find_elements(By.XPATH, "//input[@class='x-form-text x-form-field x-trigger-noedit']")[0].get_attribute("value")
+        date = driver.find_element(By.XPATH, "//input[@name='dateInput']").get_attribute("value")
+        self.timetable["date"] = datetime.strptime(date, '%d/%m/%Y').strftime('%Y-%m-%d')
+        instructor = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//span[@title='Instructor']"))).text
+
+        print(f"""Hello, { instructor } \n""")
+        print(f"Branch: { self.timetable['branch'] }")
+        print(f"Date: { self.timetable['date'] }")
+        print(f"[1] View Schedule ({ self.timetable['date'] })")
+        print(f"[2] View Schedule with Remarks/Activities ({ self.timetable['date'] })")
+        print(f"[3] Change Branch")
+        print(f"[4] Change Date")
+        print(f"[0] Logout")
+        choice = input("Choose an option [1,2,3,4,0] then press [ENTER] ")
+
+        if choice == "0":
+            # TODO: logout function
+            pass
+        elif choice == "1":
+            # TODO: extract timetable information
+            pass
+        elif choice == "2":
+            # TODO: extract timetable information and student remarks
+            pass
+        elif choice == "3":
+            self.change_branch()
+            self.timetable_menu()
+        elif choice == "4":
+            self.change_date()
+            self.timetable_menu()
+        else:
+            self.timetable_menu()
 
     def main_menu(self, msg=None):
         os.system("cls")
